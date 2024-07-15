b1519d9ed880e79a30b460f93dea9cb50c8283f6
MartinH-test <martin.hrinak@dell.com>
2024-07-15 12:58:09 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,28 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+ my_matrix <- matrix(c(1, 0, 1, 0, 4, 0, 3, 0, 4), ncol = 3, nrow = 3)
 
-}
+ makeCacheMatrix <- function(x = matrix()) {
+   if (ncol(x) == nrow(x) && det(x) != 0) {
+     m <- NULL
+     set <- function(y) {
+       x <<- y
+       m <<- NULL
+     }
+     get <- function() x
+     setinverse <- function() m <<- solve(x)
+     getinverse <- function() m
+     list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
+   } else {
+     return("The matrix is not invertible.")
+   }
+ }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+cache_matrix <- makeCacheMatrix(my_matrix)
+ cache_matrix$get()
+ cache_matrix$setinverse()
+ cache_matrix$getinverse()
